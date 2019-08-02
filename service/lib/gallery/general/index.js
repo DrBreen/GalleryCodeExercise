d5737638595e6563a155feff2badb7e1b06ae7ce
Alexander Leontev <alexander.leontev@carlsonwagonlit.com>
2019-08-02 23:42:52 +0300

    Create gallery document if it's missing

--- service/lib/gallery/general/index.js
+++ service/lib/gallery/general/index.js
@@ -7,18 +7,30 @@ const getGallery = async (galleryId) => await queryOneFromCollection(collectionN
 
 const getImages = async (galleryId) => {
     const gallery = await getGallery(galleryId);
+
+    if (!gallery) {
+        return;
+    }
+
     return gallery.images;
 };
 
 const addImage = async (galleryId, imageId) => {
-    //TODO: add gallery if it's missing
-    //TODO; add images if it's missing
     if (!await existsInStorage(imageId)) {
         throw new Error(`Trying to add non-existent image ${imageId} to gallery ${galleryId}`);
     }
 
-    const gallery = await getGallery(galleryId);
-    gallery.images.push(imageId);
+    let gallery = await getGallery(galleryId);
+    if (gallery) {
+        gallery.images.push(imageId);
+    } else {
+        gallery = {
+            galleryId,
+            images: [
+                imageId
+            ]
+        };
+    }
 
     await replaceOneInCollection(collectionName, { galleryId }, gallery);
 };
