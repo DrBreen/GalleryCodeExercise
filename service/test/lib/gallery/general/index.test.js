21b88ba269847805f53f317eb46bf06a259057ea
Alexander Leontev <alexander.leontev@carlsonwagonlit.com>
2019-08-03 01:02:32 +0300

    Add tests for addImage

--- service/test/lib/gallery/general/index.test.js
+++ service/test/lib/gallery/general/index.test.js
@@ -1,15 +1,23 @@
 const { expect } = require('chai');
+const proxyquire = require('proxyquire');
 
-const { getImages, addImage } = require('../../../../lib/gallery/general');
+//mock storage method that checks for existance of images so that it will return true for specific names
+const SUT = proxyquire('../../../../lib/gallery/general', {
+    '../../storage' : {
+        existsInStorage: (name) => {
+            return name === 'exists';
+        }
+    }
+});
 const { initMongo } = require('../../../../lib/mongo-connector');
-
+const { getImages, addImage } = SUT;
 const { MongoConfig, MongoClient } = require('../../../helpers/mock-mongo-client');
 
 describe('lib.gallery.general', () => {
 
-
     describe('getImages', () => {
 
+        //mock Mongo query result
         const initMongoWithGalleryMockData = async (gallery) => {
             await initMongo(MongoClient({
                 findOne: () => Promise.resolve(gallery)
@@ -17,7 +25,6 @@ describe('lib.gallery.general', () => {
         };
 
         it('Should return nothing if galleryId does not exist', async () => {
-
             await initMongoWithGalleryMockData(null);
 
             const images = await getImages(0);
@@ -25,7 +32,6 @@ describe('lib.gallery.general', () => {
         });
 
         it('Should return empty array if galleryId exists without images', async () => {
-
             await initMongoWithGalleryMockData({ galleryId: 0, images: [] });
 
             const images = await getImages(0);
@@ -34,14 +40,75 @@ describe('lib.gallery.general', () => {
         });
 
         it('Should return array if galleryId exists with images', async () => {
-
-            const expectedImages = ['1', '2', '3'];
             await initMongoWithGalleryMockData({ galleryId: 0, images: expectedImages });
 
+            const expectedImages = ['1', '2', '3'];
             const images = await getImages(0);
             expect(images).to.equal(expectedImages);
         });
 
+    });
+
+    describe('addImage', () => {
+
+        //mock Mongo store - if we have something in storage already, return it as result of query,
+        //if not - return null,
+        //likewise imitate replacement of object in Mongo
+        const initMongoWithGalleryMockStorage = async (storage) => {
+            await initMongo(MongoClient({
+                findOne: () => {
+                    if (storage.length > 0) {
+                        return Promise.resolve(storage[0])
+                    } else {
+                        return Promise.resolve(null)
+                    }
+                },
+                replaceOne: (query, document) => {
+                    if (storage.length > 0) {
+                        storage.length = 0;
+                    }
+
+                    storage.push(document);
+                }
+            }), MongoConfig);
+        };
+
+        it('Should fail when trying to add non-existent image', async () => {
+            try {
+                await addImage(0, 'nonexistent');
+                throw new Error('Exception was expected');
+            } catch(err) {
+                expect(err.message).to.equal('Trying to add non-existent image nonexistent to gallery 0');
+            }
+        });
+
+        it('Should create gallery document from scratch if it\'s missing', async () => {
+            const storage = [];
+            await initMongoWithGalleryMockStorage(storage);
+            await addImage(0, 'exists');
+
+            expect(storage).to.have.lengthOf(1);
+
+            const gallery = storage[0];
+            expect(gallery.galleryId).to.equal(0);
+            expect(gallery.images).to.deep.equal(['exists']);
+        });
+
+        it('Should successfully add image to gallery document', async () => {
+            const storage = [{
+                galleryId: 0,
+                images: ['test']
+            }];
+
+            await initMongoWithGalleryMockStorage(storage);
+            await addImage(0, 'exists');
+
+            expect(storage).to.have.lengthOf(1);
+
+            const gallery = storage[0];
+            expect(gallery.galleryId).to.equal(0);
+            expect(gallery.images).to.deep.equal(['test', 'exists']);
+        });
     });
 
 });
