5baf62d52f1128b2a6ba9f4865a4dd6c90142e58
Alexander Leontev <alexander.leontev@carlsonwagonlit.com>
2019-08-03 18:21:35 +0300

    Add count and offset params to image list service to support paging

--- service/lib/gallery/list/service.js
+++ service/lib/gallery/list/service.js
@@ -1,7 +1,17 @@
 const { getImages } = require('../general');
 
-const getGallery = async (galleryId) => {
-    return await getImages(galleryId);
+const getGallery = async (galleryId, offset, count) => {
+    const images = await getImages(galleryId);
+
+    let returnedImages;
+
+    if (offset !== undefined && count !== undefined) {
+        returnedImages = images.slice(Math.max(0, offset), Math.min(offset + count, images.length));
+    } else {
+        returnedImages = images;
+    }
+
+    return returnedImages;
 };
 
 Object.assign(module.exports, {
--- service/test/lib/gallery/list/service.test.js
+++ service/test/lib/gallery/list/service.test.js
@@ -1,7 +1,7 @@
 const proxyquire = require('proxyquire');
 const { expect } = require('chai');
 
-const testImages = ['1', '2', '3'];
+const testImages = ['1', '2', '3', '4', '5', '6'];
 const SUT = proxyquire('../../../../lib/gallery/list/service', {
     '../general' : {
         getImages: async () => {
@@ -14,9 +14,31 @@ const { getGallery } = SUT;
 
 describe('lib.gallery.list.service', () => {
     describe('getGallery', () => {
-        it('should pass result of call from getImages to the caller', async () => {
-            const actual = await getGallery();
+
+        it('should pass result of call from getImages to the caller when no offset is provided', async () => {
+            const actual = await getGallery(0);
             const expected = testImages;
+
+            expect(actual).to.deep.equal(expected);
+        });
+
+        it('should return correct array when offset and count are provided and are within bounds', async () => {
+            const actual = await getGallery(0, 1, 3);
+            const expected = ['2', '3', '4'];
+
+            expect(actual).to.deep.equal(expected);
+        });
+
+        it('should return correct array when offset and count are provided and offset is out of bounds', async () => {
+            const actual = await getGallery(0, 6, 3);
+            const expected = [];
+
+            expect(actual).to.deep.equal(expected);
+        });
+
+        it('should return correct array when offset and count are provided and count is larger than an array', async () => {
+            const actual = await getGallery(0, 2, 100);
+            const expected = ['3', '4', '5', '6'];
 
             expect(actual).to.deep.equal(expected);
         });
